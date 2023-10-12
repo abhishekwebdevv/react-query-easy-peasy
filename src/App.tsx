64d791d0794dbbcca8797edf5af5e8d55b1adac7
abhishek <abhishekdhakad61@gmail.com>
2023-10-12 14:47:58 +0530

    tanstack query test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
